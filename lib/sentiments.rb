245e55561a38b1aa6b8d6a4b4c891f63283ad68d
Pulkit Kathuria <kevincobain2000@USER-no-MacBook-Air.local>
2013-12-15 00:58:28 +0900

    Json Loaded, constants defined, scoring initialized

--- lib/sentiments.rb
+++ lib/sentiments.rb
@@ -1,5 +1,55 @@
 require "sentiments/version"
-
+require 'json'
 module Sentiments
-  # Your code goes here...
+	class Classifier
+        MIN_TOKEN_LENGTH = 1
+        MAX_TOKEN_LENGTH = 15
+		def initialize
+			@neg_words    = load_json('neg')
+			@pos_words    = load_json('pos')
+			@neu_words    = load_json('neu')
+			@prefix_words = load_json('prefix')
+			@ignore_words = load_json('ignore')
+
+			@categories = ['pos', 'neg', 'neu']
+
+			# TODO set dictionary
+		end
+
+		def score(sentence)
+			scores = {}
+			@prefix_words.each do |pw|
+				# Search if that prefix is in the sentence
+				if sentence.index(pw)
+					# Remove the white space after the negative prefix
+					sentence.sub! pw+" ", pw
+				end
+			end
+			tokens = sentence.split()
+			total_score = 0
+
+			@categories.each do |category|
+				scores[category] = 1
+				tokens.each do |token|
+					if (token.length > MIN_TOKEN_LENGTH && token.length < MAX_TOKEN_LENGTH && !(@ignore_words.include? token))
+						puts
+					end
+				end
+			end
+
+
+
+			return scores
+		end
+		private
+		def load_json(filename)
+			return JSON.parse(IO.read('sentiments/data/' +filename +'.json'))
+		end
+	end
 end
+
+c = Sentiments::Classifier.new
+puts c.score("nice good bad isn't it")
+
+
+
--- sentiments.gemspec
+++ sentiments.gemspec
@@ -6,11 +6,11 @@ require 'sentiments/version'
 Gem::Specification.new do |spec|
   spec.name          = "sentiments"
   spec.version       = Sentiments::VERSION
-  spec.authors       = ["TODO: Write your name"]
-  spec.email         = ["TODO: Write your email address"]
-  spec.description   = %q{TODO: Write a gem description}
-  spec.summary       = %q{TODO: Write a gem summary}
-  spec.homepage      = ""
+  spec.authors       = ["Pulkit Kathuria"]
+  spec.email         = ["kevincobain2000@gmail.com"]
+  spec.description   = %q{Sentiment Analysis with negation}
+  spec.summary       = %q{Sentiment Analysis with negation}
+  spec.homepage      = "http://www8355ue.sakura.ne.jp/?action_classifiers_sentiment=true"
   spec.license       = "MIT"
 
   spec.files         = `git ls-files`.split($/)
@@ -20,4 +20,5 @@ Gem::Specification.new do |spec|
 
   spec.add_development_dependency "bundler", "~> 1.3"
   spec.add_development_dependency "rake"
+  spec.add_development_dependency "json"
 end
