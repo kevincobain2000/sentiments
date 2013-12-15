48c72b5df48ff63b3c4387280c708b6aa99a445f
Pulkit Kathuria <kevincobain2000@USER-no-MacBook-Air.local>
2013-12-15 12:45:49 +0900

    Scoring working

--- lib/sentiments.rb
+++ lib/sentiments.rb
@@ -4,17 +4,29 @@ module Sentiments
   class Classifier
     MIN_TOKEN_LENGTH = 1
     MAX_TOKEN_LENGTH = 15
+    PRIOR_SCORE = 0.33
     def initialize
-      @neg_words    = load_json('neg')
-      @pos_words    = load_json('pos')
-      @neu_words    = load_json('neu')
+      # @neg_words    = load_json('neg')
+      # @pos_words    = load_json('pos')
+      # @neu_words    = load_json('neu')
       @prefix_words = load_json('prefix')
       @ignore_words = load_json('ignore')
 
       @categories = ['pos', 'neg', 'neu']
 
-      # TODO set dictionary
-    end
+      @dictionary = {}
+      @doc_count = 0
+      @token_count = 0
+      @category_tok_count = {'pos' => 0, 'neg' => 0, 'neu' => 0}
+      @category_doc_count = {'pos' => 0, 'neg' => 0, 'neu' => 0}
+
+      @categories.each do |category|
+        if (!set_dictionary(category))
+          raise "Unable to Set Dictionaries"
+        end #if
+      end
+
+    end #initialize
 
     def score(sentence)
       scores = {}
@@ -25,29 +37,70 @@ module Sentiments
           sentence.sub! pw+" ", pw
         end
       end
-      tokens = sentence.split()
+      tokens = tokenize(sentence)
       total_score = 0
 
       @categories.each do |category|
         scores[category] = 1
         tokens.each do |token|
           if (token.length > MIN_TOKEN_LENGTH && token.length < MAX_TOKEN_LENGTH && !(@ignore_words.include? token))
-            puts
-          end
-        end
-      end
+
+            # If Token is not in our dictionary, don't do anything
+            if(@dictionary[token].nil?)
+              break
+            end #if
+
+            if (@dictionary[token][category].nil?)
+              count = 0
+            else
+              count = @dictionary[token][category]
+            end # elseif
+            scores[category] *= (count + 1)
+          end #if
+        end #tokens
+        scores[category] = PRIOR_SCORE * scores[category]
+      end #categories
+
+      @categories.each do |category|
+        total_score += scores[category]
+      end #categories
+
+      @categories.each do |category|
+        scores[category] = scores[category]/total_score
+      end #categories
 
       return scores
-    end
+    end #score
+
     private
+
     def load_json(filename)
       return JSON.parse(IO.read('sentiments/data/' +filename +'.json'))
     end
-  end
-end
 
-c = Sentiments::Classifier.new
-puts c.score("nice good bad isn't it")
+    def set_dictionary(category)
+      words = load_json(category)
+      words.each do |word|
+        @doc_count += 1
+        @category_doc_count[category] += 1
 
+        if (@dictionary[word].nil?)
+          @dictionary[word] = {}
+          @dictionary[word][category] = 1
+        end
+        @category_tok_count[category] += 1
+        @token_count += 1
+      end
+      return true
+    end #set_dictionary
 
+    def tokenize(sentence)
+      sentence = sentence.downcase
+      return sentence.split()
+    end #tokenize
 
+  end #class
+end #module
+
+classifier = Sentiments::Classifier.new
+puts classifier.score("lovely performance")
